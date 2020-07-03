de8f84c22b84ee01498b710a0697c9749bf55902
callumgillies <callumgillies@nhs.net>
2020-07-03 16:28:05 +0100

    pep 8 compliant apart from library import message

--- spot_commissioning.py
+++ spot_commissioning.py
@@ -6,6 +6,7 @@ import xlsxwriter
 import test.test_version
 from logos_module import *
 
+
 def main():
 
     test.test_version.check_version()
@@ -34,7 +35,7 @@ def main():
 
     image_list = sorted(image_list, key=lambda f: int(os.path.splitext(f)[0]))
 
-    active_script = ActiveScript(os.path.join(dir,'activescript.txt'))
+    active_script = ActiveScript(os.path.join(dir, 'activescript.txt'))
     resolution = [active_script.CameraHRatio, active_script.CameraVRatio]
 
     workbook = xlsxwriter.Workbook(os.path.join(dir,
@@ -43,7 +44,7 @@ def main():
                                    )
 
     for key in image_list:
-        spot = SingleSpotTif(os.path.join(dir,key))
+        spot = SingleSpotTif(os.path.join(dir, key))
 
         # array = image_to_array(os.path.join(dir,key)) # A numpy array
         # center = find_centre(array) # A List, [CenterRow, CenterCol]
@@ -54,24 +55,23 @@ def main():
         y_half = int(spot.y_norm.shape[1]/2)
         y_half_plus_ten = 10+int(spot.y_norm.shape[1]/2)
 
-        x_prof_log = np.asarray([spot.x_norm[0],np.log10(spot.x_norm[1])])
-        y_prof_log = np.asarray([spot.y_norm[0],np.log10(spot.y_norm[1])])
+        x_prof_log = np.asarray([spot.x_norm[0], np.log10(spot.x_norm[1])])
+        y_prof_log = np.asarray([spot.y_norm[0], np.log10(spot.y_norm[1])])
 
-        x_prof_log_l = x_prof_log[:,0:x_half_plus_ten]
-        x_prof_log_r = x_prof_log[:,-x_half_plus_ten::]
-        y_prof_log_l = y_prof_log[:,0:y_half_plus_ten]
-        y_prof_log_r = y_prof_log[:,-y_half_plus_ten::]
-
+        x_prof_log_l = x_prof_log[:, 0:x_half_plus_ten]
+        x_prof_log_r = x_prof_log[:, -x_half_plus_ten::]
+        y_prof_log_l = y_prof_log[:, 0:y_half_plus_ten]
+        y_prof_log_r = y_prof_log[:, -y_half_plus_ten::]
 
         x_params_l = log_2_gaus_shift_fit(x_prof_log_l)
         x_params_r = log_2_gaus_shift_fit(x_prof_log_r)
         y_params_l = log_2_gaus_shift_fit(y_prof_log_l)
         y_params_r = log_2_gaus_shift_fit(y_prof_log_r)
 
-        x_fit_l = 10**log_2_gaus_shift_func(x_prof_log_l[0],*x_params_l)
-        x_fit_r = 10**log_2_gaus_shift_func(x_prof_log_r[0],*x_params_r)
-        y_fit_l = 10**log_2_gaus_shift_func(y_prof_log_l[0],*y_params_l)
-        y_fit_r = 10**log_2_gaus_shift_func(y_prof_log_r[0],*y_params_r)
+        x_fit_l = 10**log_2_gaus_shift_func(x_prof_log_l[0], *x_params_l)
+        x_fit_r = 10**log_2_gaus_shift_func(x_prof_log_r[0], *x_params_r)
+        y_fit_l = 10**log_2_gaus_shift_func(y_prof_log_l[0], *y_params_l)
+        y_fit_r = 10**log_2_gaus_shift_func(y_prof_log_r[0], *y_params_r)
 
         x_model = [spot.x_norm[0], np.concatenate([x_fit_l[0:x_half],
                                                    x_fit_r[-x_half:]
@@ -88,75 +88,76 @@ def main():
         # plt.plot(x_prof_log_r[0],10**x_fit_r)
         # plt.show()
         energy = key.split('.')[0]
-        worksheet=workbook.add_worksheet(energy)
+        worksheet = workbook.add_worksheet(energy)
         worksheet.write('B2', 'X Distance mm')
-        worksheet.write_column('B3',spot.x_norm[0])
+        worksheet.write_column('B3', spot.x_norm[0])
         worksheet.write('C2', 'X Data')
-        worksheet.write_column('C3',spot.x_norm[1])
+        worksheet.write_column('C3', spot.x_norm[1])
         worksheet.write('D2', 'X Model')
         worksheet.write_column('D3', x_model[1])
         worksheet.write('E2', 'Y Distance mm')
-        worksheet.write_column('E3',spot.y_norm[0])
+        worksheet.write_column('E3', spot.y_norm[0])
         worksheet.write('F2', 'Y Data')
-        worksheet.write_column('F3',spot.y_norm[1])
+        worksheet.write_column('F3', spot.y_norm[1])
         worksheet.write('G2', 'Y Model')
         worksheet.write_column('G3', y_model[1])
 
-        worksheet.set_column(1,6,14)
+        worksheet.set_column(1, 6, 14)
 
         chart1 = workbook.add_chart({'type': 'scatter'})
         chart1.add_series({
             'name':       'Data',
-            'categories': "".join("="+energy+"!$B$3:$B$"+
-                                  str(2+len(spot.x_norm[0]))),
-            'values':     "".join("="+energy+"!$C$3:$C$"+
-                                  str(2+len(spot.x_norm[1]))),
-            'line': {'color':'red', 'width': 1,},
-            'marker': {'type':'none'},
+            'categories': "".join("=" + energy + "!$B$3:$B$"
+                                  + str(2 + len(spot.x_norm[0]))),
+            'values':     "".join("=" + energy + "!$C$3:$C$"
+                                  + str(2 + len(spot.x_norm[1]))),
+            'line': {'color': 'red', 'width': 1, },
+            'marker': {'type': 'none'},
         })
         chart1.add_series({
             'name':       'Model',
-            'categories': "".join("="+energy+"!$B$3:$B$"+
-                                  str(2+len(spot.x_norm[0]))),
-            'values':     "".join("="+energy+"!$D$3:$D$"+
-                                  str(2+len(x_model[1]))),
-            'line': {'color':'blue', 'width': 1,},
-            'marker': {'type':'none'},
+            'categories': "".join("=" + energy + "!$B$3:$B$"
+                                  + str(2+len(spot.x_norm[0]))),
+            'values':     "".join("=" + energy + "!$D$3:$D$"
+                                  + str(2+len(x_model[1]))),
+            'line': {'color': 'blue', 'width': 1, },
+            'marker': {'type': 'none'},
         })
-        chart1.set_title ({'name': 'X Profile'})
+        chart1.set_title({'name': 'X Profile'})
         chart1.set_x_axis({'name': 'X_Coordinate (mm)'})
         chart1.set_y_axis({'name': 'Relative intensity'})
         chart1.set_style(11)
-        chart1.set_size({'x_scale':1.55,'y_scale':1.6})
+        chart1.set_size({'x_scale': 1.55, 'y_scale': 1.6})
         worksheet.insert_chart('I2', chart1)
-
 
         chart2 = workbook.add_chart({'type': 'scatter'})
         chart2.add_series({
             'name':       'Data',
-            'categories': "".join("="+energy+"!$E$3:$E$"+
-                                  str(2+len(spot.y_norm[0]))),
-            'values':     "".join("="+energy+"!$F$3:$F$"+
-                                  str(2+len(spot.y_norm[1]))),
-            'line': {'color':'red', 'width': 1,},
-            'marker': {'type':'none'},
+            'categories': "".join("=" + energy + "!$E$3:$E$"
+                                  + str(2+len(spot.y_norm[0]))),
+            'values':     "".join("=" + energy + "!$F$3:$F$"
+                                  + str(2+len(spot.y_norm[1]))),
+            'line': {'color': 'red', 'width': 1, },
+            'marker': {'type': 'none'},
         })
         chart2.add_series({
             'name':       'Model',
-            'categories': "".join("="+energy+"!$E$3:$E$"+
-                                  str(2+len(spot.y_norm[0]))),
-            'values':     "".join("="+energy+"!$G$3:$G$"+
-                                  str(2+len(y_model[1]))),
-            'line': {'color':'blue', 'width': 1,},
-            'marker': {'type':'none'},
+            'categories': "".join("=" + energy + "!$E$3:$E$"
+                                  + str(2 + len(spot.y_norm[0]))),
+            'values':     "".join("=" + energy + "!$G$3:$G$"
+                                  + str(2 + len(y_model[1]))),
+            'line': {'color': 'blue', 'width': 1, },
+            'marker': {'type': 'none'},
         })
-        chart2.set_title ({'name': 'Y Profile'})
+        chart2.set_title({'name': 'Y Profile'})
         chart2.set_x_axis({'name': 'Y_Coordinate (mm)'})
         chart2.set_y_axis({'name': 'Relative intensity'})
         chart2.set_style(11)
-        chart2.set_size({'x_scale':1.55,'y_scale':1.6})
+        chart2.set_size({'x_scale': 1.55, 'y_scale': 1.6})
         worksheet.insert_chart('I25', chart2)
 
     workbook.close()
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
     main()
